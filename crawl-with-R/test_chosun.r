cc31e4fff684e32eb9ec130e50ca4e19aea9fa88
beerabae <bt8283@gmail.com>
2020-05-12 22:23:47 +0900

    add 200504~200505 all_chosun_news title

--- crawl-with-R/test_chosun.r
+++ crawl-with-R/test_chosun.r
@@ -7,15 +7,20 @@ library(rvest)
 
 #target url : "http://news.chosun.com/svc/list_in/list_title.html?indate=20200501&pn=1"
 
-base_url<- "http://news.chosun.com/svc/list_in/list_title.html?indate=20200504&pn="
-base_url
+base_url1<- "http://news.chosun.com/svc/list_in/list_title.html?indate="
+base_url2<- "&pn="
+base_url1
+base_url2
 
+#일정기간 중  조선일보 전체기사 가져오기 (일 발간수 최대 1000개로 가정) 
+date<-20200504
 page <- 1
 all_chosun_news <- c()
 
-while(page< 50){
+while(date <= 20200505){
+  while(page< 50){
   
-  chosun_url <- paste0(base_url, page)
+  chosun_url <- paste0(base_url1,date,base_url2,page)
   
   chosun_html <- read_html(chosun_url)
   
@@ -30,5 +35,7 @@ while(page< 50){
   write(all_chosun_news, "test_chosun.txt")
 
   page <- page +1
-  
+  } 
+  date <- date +1
+  page<- 1
 }
